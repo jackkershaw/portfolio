22920f72b377d678624acd5f5e17bbfe2714ed95
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-12 17:20:50 +0000

    edit custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        blue: "#002FA7",
-        white: "#FFFFFF",
-        green: "#32772B",
-        pink: "#DA3A7F",
-        grey: "#767684",
+        // adding my favourite blue, yves klein blue //
+        ykb: {
+          DEFAULT: "#002FA7",
+          50: "#608CFF",
+          100: "#4B7EFF",
+          200: "#2260FF",
+          300: "#0046F9",
+          400: "#003AD0",
+          500: "#002FA7",
+          600: "#001F6F",
+          700: "#000F37",
+          800: "#000000",
+          900: "#000000",
+          950: "#000000",
+        },
       },
     },
   },
