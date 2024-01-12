732b72c1707e089008475ff5809036e02f4cd135
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-12 17:30:11 +0000

    editing to line up with footer

--- src/header.jsx
+++ src/header.jsx
@@ -1,7 +1,7 @@
 function Header({onHeaderClick}) {
     return (
         <>
-        <header className="bg-ykb-500 text-white p-4 min-w-100">
+        <header className="bg-ykb-500 text-white py-4 min-w-100">
         <navbar className="mx-10 container flex  justify-between items-center flex-col lg:flex-row sm:text-xl md:text-3xl">
               <h1 onClick={() => onHeaderClick('openingPage')} className="hover:text-ykb-50 active:text-ykb-200" >About</h1>
               <h1 onClick={() => onHeaderClick('coding')} className="hover:text-ykb-50 active:text-ykb-200" >Coding</h1>
