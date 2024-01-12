a0dab030b8fbc432eedb30bc8ec10abc65446175
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-12 17:20:32 +0000

    edit header design

--- src/header.jsx
+++ src/header.jsx
@@ -1,19 +1,13 @@
-import DarkMode from "./darkmode";
-
-
 function Header({onHeaderClick}) {
     return (
         <>
-        <header>
-      <navbar id="navbar" className="text-white">
-        <section id="navbar-text">
-          <h1 onClick={() => onHeaderClick('openingPage')}>About</h1>
-          <h1 onClick={() => onHeaderClick('coding')}>Coding</h1>
-          <h1 onClick={() => onHeaderClick('design')}>Design</h1>
-          <h1 onClick={() => onHeaderClick('videos')}>Video</h1>
-          <DarkMode></DarkMode>
-        </section>
-      </navbar>
+        <header className="bg-ykb-500 text-white p-4 min-w-100">
+        <navbar className="mx-10 container flex  justify-between items-center flex-col lg:flex-row sm:text-xl md:text-3xl">
+              <h1 onClick={() => onHeaderClick('openingPage')} className="hover:text-ykb-50 active:text-ykb-200" >About</h1>
+              <h1 onClick={() => onHeaderClick('coding')} className="hover:text-ykb-50 active:text-ykb-200" >Coding</h1>
+              <h1 onClick={() => onHeaderClick('design')} className="hover:text-ykb-50 active:text-ykb-200" >Design</h1>
+              <h1 onClick={() => onHeaderClick('videos')} className="hover:text-ykb-50 active:text-ykb-200" >Video</h1>
+        </navbar>
       </header>
     </>
     )
