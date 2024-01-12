325da133b4b727940fe97de719a8fc2db0ba4f37
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-12 17:20:38 +0000

    edit footer design

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,12 +1,11 @@
 function Footer() {
     return (
       <>
-      <footer>
-        <section id="footer-text">
-          <h2 className="left" id="bottom-to-top">
-            <a href="#navbar">back to top ^ </a>
+      <footer className="bg-ykb-500 text-white text-2xl py-4 fixed bottom-0 w-full">
+      <navbar className="mx-10 container flex flex-col justify-between items-center sm:flex-row ">
+          <h2> back to top ^
           </h2>
-        </section>
+      </navbar>
       </footer>
       </>
     )
