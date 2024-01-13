74e6aea58250fdc30758daf785a10fe732b547f1
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-13 16:26:53 +0000

    add contact form structure from old portfolio

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,11 +1,47 @@
 function Contact() {
-    return (
-        <>
-        <p className="text-5xl text-ykb-700 mx-10 py-10">
-        I'm currently looking for new projects. If you'd like to work with me, feel free to reach out!
-    </p>
+  return (
+    <>
+      <p className="mx-10 py-10 text-5xl text-ykb-700">
+        I'm currently looking for new projects. If you'd like to work with me,
+        feel free to reach out!
+      </p>
+      <form action="https://formsubmit.co/jcode689@gmail.com" method="POST">
+        <h4>Want to send me a message? Get in touch below.</h4>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Your name"
+          required
+        />
+        <br />
+        <br />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Your email"
+          required
+        />
+        <br />
+        <br />
+        <textarea
+          id="message"
+          name="message"
+          placeholder="How can I help?"
+          required
+          rows="4"
+        ></textarea>
+        <br />
+        <br />
+        <button type="submit" value="Submit">
+          Submit
+        </button>
+        <br />
+        <br />
+      </form>
     </>
-    )
+  );
 }
 
 export default Contact;
