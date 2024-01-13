f6d4d513fb5c09a9807de5c20d9f9d4aef52e984
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-13 12:24:13 +0000

    formatting with prettier :)

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,15 +1,13 @@
 function Footer() {
-    return (
-      <>
+  return (
+    <>
       <footer className="bg-ykb-500 text-white text-2xl py-4 fixed bottom-0 w-full">
-      <navbar className="mx-10 container flex flex-col justify-between items-center sm:flex-row ">
-          <h2> back to top ^
-          </h2>
-      </navbar>
+        <navbar className=" md:mx-5 lg:mx-10 container flex flex-col justify-between items-center sm:flex-row ">
+          <h2> back to top ^</h2>
+        </navbar>
       </footer>
-      </>
-    )
+    </>
+  );
 }
-
 
 export default Footer;
--- src/js from old portfolio.js
+++ src/js from old portfolio.js
@@ -1,10 +1,10 @@
 // type out big words on launch //
 
-document.addEventListener("DOMContentLoaded", function () {
-  const typedText = document.getElementById("big-introduction");
+document.addEventListener('DOMContentLoaded', function () {
+  const typedText = document.getElementById('big-introduction');
   function typeOut() {
     const originalText = typedText.textContent;
-    typedText.textContent = "";
+    typedText.textContent = '';
     for (let i = 0; i < originalText.length; i++) {
       let randomDelay = Math.random() * 100 * i;
       setTimeout(() => {
