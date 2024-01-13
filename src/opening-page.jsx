a5536b0fe2aaf08dcab24093e5dbe7101865b9cb
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-13 10:48:45 +0000

    edit opening page text

--- src/opening-page.jsx
+++ src/opening-page.jsx
@@ -1,9 +1,9 @@
 function OpeningPage() {
     return (
         <>
-      <p className="text-5xl text-ykb-700 mx-10 py-10">
-        Hi there! I'm Jack, a London-based designer and developer.
-        I've spent the last year learning HTML, CSS, Javascript, Python and React.
+      <p className="md:text-5xl sm:text-4xl text-ykb-700 mx-10 py-10">
+        Hi there! I'm Jack, a London-based designer and developer. I've spent the past year and a half teaching myself the fundamentals of development, including a twelve-week bootcamp.
+        So far I've learnt HTML, CSS, Javascript, Python, React and Tailwind CSS.
       </p>
     </>
     )
