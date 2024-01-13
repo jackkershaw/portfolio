730f4464898f8909f3cf202140a75104bc11349a
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-13 16:50:41 +0000

    make footer sticky rather than fixed

--- src/footer.jsx
+++ src/footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
 
   return (
     <>
-      <footer className="fixed bottom-0 w-full bg-ykb-500 py-4 text-2xl text-white">
+      <footer className="sticky bottom-0 w-full bg-ykb-500 py-4 text-2xl text-white">
         <navbar className=" container flex flex-col items-center justify-between sm:flex-row md:mx-5 lg:mx-10 ">
           <h2
             onClick={scrollToTop}
