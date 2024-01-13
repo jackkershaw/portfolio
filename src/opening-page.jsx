2c855bd1ad12c4eecf5715eb96eef8e79d535dc2
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-13 16:27:06 +0000

    add opening text

--- src/opening-page.jsx
+++ src/opening-page.jsx
@@ -1,12 +1,15 @@
 function OpeningPage() {
-    return (
-        <>
-      <p className="md:text-5xl sm:text-4xl text-ykb-700 mx-10 py-10">
-        Hi there! I'm Jack, a London-based designer and developer. I've spent the past year and a half teaching myself the fundamentals of development, including a twelve-week bootcamp.
-        So far I've learnt HTML, CSS, Javascript, Python, React and Tailwind CSS.
+  return (
+    <>
+      <p className="mx-10 py-10 text-2xl text-ykb-700 md:text-5xl ">
+        Hi there! I'm Jack, a London-based designer and developer. I've spent
+        the past year and a half learning the fundamentals of development,
+        including taking part in Just IT's highly selective twelve-week
+        bootcamp. So far I've learnt HTML, CSS, Javascript, Python, React and
+        Tailwind CSS.
       </p>
     </>
-    )
+  );
 }
 
-export default OpeningPage
+export default OpeningPage;
