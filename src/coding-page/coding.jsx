765dbfef788f17c92b3b483438f58fb73915ab44
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-15 14:58:43 +0000

    adding test images

--- src/coding-page/coding.jsx
+++ src/coding-page/coding.jsx
@@ -5,81 +5,90 @@ function Coding() {
       description:
         "My partner and I both like to play Wordle adjacent games so I made this website.",
       link: "https://dailychallenges.jackkershaw.net",
+      image_folder: "daily-challenges",
     },
     {
       title: "Typespotting",
       description: "I like fonts.",
       link: "https://dailychallenges.jackkershaw.net",
+      image_folder: "typespotting",
     },
     {
       title: "London Brutalist Map",
       description:
         "My partner and I both like to play Wordle adjacent games so I made this website.",
       link: "https://dailychallenges.jackkershaw.net",
+      image_folder: "brutalist-map",
     },
     {
-      title: "Daily Challenges",
+      title: "Other Project",
       description:
         "My partner and I both like to play Wordle adjacent games so I made this website.",
       link: "https://dailychallenges.jackkershaw.net",
+      image_folder: "other",
     },
   ];
 
   return (
-    <>
-      <div className="space-between bottom-30 mx-5 flex flex-col">
-        <section>
-          <p className="max-w-max py-4 text-2xl sm:text-3xl">
-            Projects I've built while learning to code. You can follow my
-            progress{" "}
-            <a
-              href="https://progress.jackkershaw.net"
-              target="_blank"
-              className="shadow-md hover:underline lg:shadow-none"
+    <div className="space-between mx-5 mb-10 flex flex-col py-10">
+      <section>
+        <p className="max-w-max py-4 text-2xl sm:text-3xl">
+          Projects I've built while learning to code. You can follow my progress{" "}
+          <a
+            href="https://progress.jackkershaw.net"
+            target="_blank"
+            className="shadow-md hover:underline lg:shadow-none"
+          >
+            here
+          </a>{" "}
+          and see some of my favorite learning resources{" "}
+          <a
+            href="https://github.com/jones58/Learning-Resources"
+            target="_blank"
+            className="shadow-md hover:underline lg:shadow-none"
+          >
+            here
+          </a>
+          .
+        </p>
+      </section>
+      <section>
+        <ol className="flex-space-between flex flex-col">
+          {projects.map((project, index) => (
+            <li
+              key={index}
+              className="flex-space-between flex flex-col lg:flex-row"
             >
-              here
-            </a>{" "}
-            and see some of my favorite learning resources{" "}
-            <a
-              href="https://github.com/jones58/Learning-Resources"
-              target="_blank"
-              className="shadow-md hover:underline lg:shadow-none"
-            >
-              here
-            </a>
-            .
-          </p>
-        </section>
-        <section>
-          <ol>
-            {projects.map((project, index) => (
-              <div>
-                <iframe
-                  src={project.link}
-                  className="aspect-[1600/1093] py-4"
-                ></iframe>
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
-                <a
-                  href={project.link}
-                  target="_blank"
-                  className="shadow-md hover:underline lg:shadow-none"
-                >
-                  Click here to view full site
-                </a>
-                <iframe
-                  src={project.link}
-                  className="aspect-[946/2048] py-4"
-                ></iframe>
-              </div>
-            ))}
-          </ol>
-        </section>
-        <section>
-          <h3>Where next?</h3>
-        </section>
-      </div>
-    </>
+              <img
+                src={`/src/coding-page/${project.image_folder}/desktop.jpg`}
+                alt={`desktop view of ${project.title}`}
+                className="aspect-[1600/1093] h-auto max-w-full py-2"
+                style={{ height: "auto" }}
+              />
+              <h3 className="text-2xl font-semibold">{project.title}</h3>
+              <p className="text-xl">{project.description}</p>
+              <a
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mb-5 text-right text-xl shadow-md hover:underline sm:mb-0 lg:shadow-none"
+              >
+                Click here to view full site
+              </a>
+              <img
+                src={`/src/coding-page/${project.image_folder}/mobile.jpg`}
+                alt={`mobile view of ${project.title}`}
+                className="hidden aspect-[946/2048] h-auto max-w-full py-2 lg:block"
+              />
+              <div className="border-l-10 mx-auto border-dotted border-ykb-500"></div>
+            </li>
+          ))}
+        </ol>
+      </section>
+      <section>
+        <h3 className="text-2xl font-semibold">Where next?</h3>
+      </section>
+    </div>
   );
 }
 
