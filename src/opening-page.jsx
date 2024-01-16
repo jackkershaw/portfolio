d94c05bd330cbd8bb7b2b2df6a8fc5666ec867ff
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-16 13:53:36 +0000

    edit style opening page

--- src/opening-page.jsx
+++ src/opening-page.jsx
@@ -1,6 +1,6 @@
 function OpeningPage() {
   return (
-    <>
+    <div className="mb-10 py-10">
       <p className="mx-10 py-10 text-2xl text-ykb-700 md:text-5xl ">
         Hi there! I'm Jack, a London-based designer and developer. I've spent
         the past year and a half learning the fundamentals of development,
@@ -8,7 +8,7 @@ function OpeningPage() {
         bootcamp. So far I've learnt HTML, CSS, Javascript, Python, React and
         Tailwind CSS.
       </p>
-    </>
+    </div>
   );
 }
 
