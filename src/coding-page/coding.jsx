b36861d86efa0077a64f789066eb3b06e374a89d
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-16 13:54:08 +0000

    edit for desktop

--- src/coding-page/coding.jsx
+++ src/coding-page/coding.jsx
@@ -3,7 +3,7 @@ function Coding() {
     {
       title: "Daily Challenges",
       description:
-        "My partner and I both like to play Wordle and Wordle-adjacent games so I made this website.",
+        "My partner and I like to play Wordle-adjacent games so I made this website.",
       link: "https://dailychallenges.jackkershaw.net",
       github_link: "https://github.com/jones58/daily-challenges",
       image_folder: "daily-challenges",
@@ -26,7 +26,7 @@ function Coding() {
       title: "London Brutalist Map",
       description:
         "My partner and I both like to play Wordle adjacent games so I made this website.",
-      link: "https://dailychallenges.jackkershaw.net",
+      link: "https://brutalistmap.jackkershaw.net/",
       github_link: "https://github.com/jones58/London-Brutalist-Map",
       image_folder: "brutalist-map",
     },
@@ -35,14 +35,14 @@ function Coding() {
       description: "This website. ",
       link: "https://www.jackkershaw.net",
       github_link: "https://github.com/jones58/portfolio-2024",
-      image_folder: "other",
+      image_folder: "portfolio",
     },
   ];
 
   return (
     <div className="space-between mx-5 mb-10 flex flex-col py-10">
       <section>
-        <p className="max-w-max py-4 text-2xl sm:text-3xl">
+        <p className="py-4 text-2xl lg:mx-5 lg:w-1/3">
           Projects I've built while learning to code. You can follow my progress{" "}
           <a
             href="https://progress.jackkershaw.net"
@@ -55,7 +55,7 @@ function Coding() {
           <a
             href="https://github.com/jones58/Learning-Resources"
             target="_blank"
-            className="shadow-md hover:underline lg:shadow-none "
+            className="shadow-md hover:underline lg:shadow-none"
           >
             here
           </a>
@@ -63,51 +63,72 @@ function Coding() {
         </p>
       </section>
       <section>
-        <ol className="flex-space-between flex flex-col">
+        <ol className="flex-space-between flex flex-col justify-center">
           {projects.map((project, index) => (
             <li
               key={index}
-              className="flex-space-between flex flex-col lg:flex-row"
+              className="flex flex-col lg:m-5 lg:grid lg:grid-cols-4 lg:border-2 lg:border-black lg:pt-6"
             >
-              <a href={project.link} target="_blank">
+              <a
+                href={project.link}
+                target="_blank"
+                className="lg:col-span-2 lg:h-[30vw] lg:px-10 lg:py-8"
+              >
                 <img
                   src={`/src/coding-page/${project.image_folder}/desktop.jpg`}
                   alt={`desktop view of ${project.title}`}
-                  className="my-2 aspect-[1600/1093] h-auto w-full border-2 border-black object-contain "
+                  className="my-2 h-auto w-full border-2 border-black object-contain lg:border-0"
                 />
               </a>
-              <div>
-                <h3 className="text-2xl font-semibold">{project.title}</h3>
-                <p className="text-xl">{project.description}</p>
+              <div className="lg:col-span-1 lg:my-8  lg:p-8">
+                <h3 className="text-2xl font-semibold lg:h-[8vw] lg:pb-5 lg:text-4xl">
+                  {project.title}
+                </h3>
+                <p className="text-xl lg:h-[10vw] lg:text-2xl">
+                  {project.description}
+                </p>
                 <a
                   href={project.link}
                   target="_blank"
-                  className="mt-4 block w-full px-4 py-2 text-center text-xl shadow-md hover:underline lg:shadow-none"
+                  className="mt-4 block w-full px-4 py-2 text-center text-xl shadow-md hover:underline lg:mt-0 lg:px-0 lg:py-2 lg:text-left lg:shadow-none
+                  "
                 >
                   View Website
                 </a>
                 <a
                   href={project.github_link}
                   target="_blank"
-                  className="mt-5 block w-full px-4 py-2 text-center text-xl shadow-md hover:underline lg:shadow-none"
+                  className="mt-5 block w-full px-4 py-2 text-center text-xl shadow-md hover:underline lg:mt-0 lg:px-0 lg:py-2 lg:text-left lg:shadow-none"
                 >
                   View Github
                 </a>
               </div>
-              <a href={project.link} target="_blank">
+              <a
+                href={project.link}
+                target="_blank"
+                className="lg:col-span-1 lg:h-[30vw]"
+              >
                 <img
                   src={`/src/coding-page/${project.image_folder}/mobile.jpg`}
                   alt={`mobile view of ${project.title}`}
-                  className="hidden aspect-[946/2048] py-2 lg:block"
+                  className="my-2 hidden h-auto max-h-[30vw] w-full object-contain lg:block "
                 />
               </a>
-              <div class="mx-auto my-4 h-10 border-8 border-dashed border-black"></div>
+              <div class="mx-auto my-4 h-10 border-8 border-dashed border-black lg:hidden"></div>
             </li>
           ))}
         </ol>
       </section>
-      <section>
-        <h3 className="text-2xl font-semibold">Where next?</h3>
+      <section className="lg:m-5 lg:grid lg:grid-cols-4 lg:border-2 lg:border-black">
+        <a
+          href="https://github.com/jones58"
+          target="_blank"
+          className="shadow-lg hover:underline lg:col-span-2 lg:col-start-3 lg:shadow-none"
+        >
+          <h3 className="mb-5 max-w-full p-5 text-center text-2xl font-semibold lg:pt-8 lg:text-left lg:text-5xl">
+            Where next?
+          </h3>
+        </a>
       </section>
     </div>
   );
