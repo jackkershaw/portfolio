8f37cab85ef5fe8ff0cdaa6ac1523a45b4efdb4e
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-23 14:07:21 +0000

    edit styling

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,9 +1,9 @@
 function Contact() {
   return (
-    <div className="mb-10 pb-10">
-      <div className="space-between mx-5 mb-10 flex flex-col pt-10">
-        <p className="text-2xl  md:mx-5 md:py-4 lg:w-1/3">
-          Get in touch here or email me directly on{" "}
+    <div className="mx-5 mb-10 py-14 ">
+      <div className="space-between mb-10 flex flex-col">
+        <p className="text-2xl  md:mx-5">
+          Get in touch here or email me directly on {""}
           <a
             href="mailto:jkershaw986@gmail.com"
             target="_blank"
@@ -16,7 +16,7 @@ function Contact() {
       <form
         action="https://formsubmit.co/jcode689@gmail.com"
         method="POST"
-        className="mx-5 flex flex-col space-y-2 border-black sm:w-1/3 md:mx-10"
+        className="my-10 flex flex-col space-y-3 border-2 border-black p-10 hover:shadow-md hover:shadow-ykb-500 md:mx-10"
       >
         <input
           type="text"
@@ -24,7 +24,7 @@ function Contact() {
           name="name"
           placeholder="Your name"
           required
-          className=" rounded border-2 p-4 focus:border-ykb focus:shadow-md"
+          className="rounded border-2 p-4 text-xl focus:border-ykb focus:shadow-md sm:p-10"
         />
         <br />
         <input
@@ -33,7 +33,7 @@ function Contact() {
           name="email"
           placeholder="Your email"
           required
-          className=" rounded border-2 p-4  focus:border-ykb focus:shadow-md"
+          className=" rounded border-2 p-4  text-xl focus:border-ykb focus:shadow-md sm:p-10"
         />
         <br />
         <textarea
@@ -42,13 +42,13 @@ function Contact() {
           placeholder="Your message"
           required
           rows="4"
-          className=" rounded border-2 p-4 focus:border-ykb focus:shadow-md"
+          className=" rounded border-2 p-4 text-xl focus:border-ykb focus:shadow-md sm:p-10"
         ></textarea>
         <br />
         <button
           type="submit"
           value="Submit"
-          className=" rounded border-4 border-ykb p-4 text-left shadow-md focus:border-ykb focus:shadow-md"
+          className="rounded border-4 border-ykb p-4 py-10 text-left text-xl font-bold hover:shadow-md hover:shadow-black focus:border-ykb focus:shadow-md sm:p-10"
         >
           Submit
         </button>
