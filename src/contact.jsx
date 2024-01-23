5d7059015f477746eed1ec0893b7caace9940b8e
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-23 14:39:32 +0000

    styling contact form

--- src/contact.jsx
+++ src/contact.jsx
@@ -18,23 +18,24 @@ function Contact() {
         method="POST"
         className="my-10 flex flex-col space-y-3 border-2 border-black p-10 hover:shadow-md hover:shadow-ykb-500 md:mx-10"
       >
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Your name"
-          required
-          className="rounded border-2 p-4 text-xl focus:border-ykb focus:shadow-md sm:p-10"
-        />
-        <br />
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Your email"
-          required
-          className=" rounded border-2 p-4  text-xl focus:border-ykb focus:shadow-md sm:p-10"
-        />
+        <div className="space-y-3  sm:grid sm:grid-cols-3 sm:space-y-0">
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Your name"
+            required
+            className=" w-full rounded border-2 p-4 text-xl focus:border-ykb focus:shadow-md sm:col-span-2 sm:mr-5 sm:w-auto sm:px-10 sm:py-6"
+          />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Your email"
+            required
+            className="w-full rounded border-2 p-4 text-xl focus:border-ykb focus:shadow-md sm:w-auto sm:px-10 sm:py-6"
+          />
+        </div>
         <br />
         <textarea
           id="message"
@@ -42,13 +43,13 @@ function Contact() {
           placeholder="Your message"
           required
           rows="4"
-          className=" rounded border-2 p-4 text-xl focus:border-ykb focus:shadow-md sm:p-10"
+          className=" rounded border-2 p-4 text-xl focus:border-ykb focus:shadow-md sm:px-10 sm:py-6"
         ></textarea>
         <br />
         <button
           type="submit"
           value="Submit"
-          className="rounded border-4 border-ykb p-4 py-10 text-left text-xl font-bold hover:shadow-md hover:shadow-black focus:border-ykb focus:shadow-md sm:p-10"
+          className="rounded border-4 border-ykb p-4 py-10 text-left text-xl font-bold hover:shadow-md hover:shadow-black focus:border-ykb focus:shadow-md sm:px-10 sm:py-6"
         >
           Submit
         </button>
