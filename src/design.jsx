728601fb0cd96d1e08944ab7fd0b1d2e8cff2e0e
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-23 15:15:13 +0000

    add descriptions for design projects

--- src/design.jsx
+++ src/design.jsx
@@ -2,26 +2,54 @@ function Design() {
   const designs = [
     {
       id: "fete2022",
-      description: "design",
+      description:
+        "Poster designed as part of the broader identity and logo design I completed for Fête de L'Humanité 2022 - a festival drawing crowds of over 500,000 in France.",
     },
     {
       id: "leaves",
-      description: "design ",
+      description:
+        "Self-initiated poster made by gathering leaves, scanning and manipulating them in Photoshop.",
     },
     {
       id: "cpc",
-      description: "design",
+      description:
+        "Logo and brand identity for the Centre for Progressive Change.",
     },
-    { id: "pelicula", description: "design " },
-    { id: "peoplesvaccine", description: "design " },
-    { id: "safesickpay", description: "design " },
-    { id: "scotland-tuc", description: "design " },
+    {
+      id: "pelicula",
+      description:
+        "An open-source typeface inspired by Poster Collective posters at May Day Rooms.",
+    },
+    {
+      id: "peoplesvaccine",
+      description:
+        "Poster design for the People's Vaccine campaign, advocating for global access to COVID-19 vaccines.",
+    },
+    {
+      id: "safesickpay",
+      description:
+        "Designs for Safe Sick Pay, a policy research project to raise the rate of statuory sick pay.",
+    },
+    {
+      id: "scotland-tuc",
+      description:
+        "Leaflet, beer mat and pins for Scotland TUC, an organisation in Scotland providing guidance to workers and employers on how to return to work after the COVID-19 lockdowns.",
+    },
     {
       id: "fete2023",
-      description: "design",
+      description:
+        "Poster designed as part of the broader identity and logo design I completed for Fête de L'Humanité 2023 - a festival drawing crowds of over 600,000 in France.",
     },
-    { id: "screenburn", description: "design " },
-    { id: "fairwork", description: "design " },
+    {
+      id: "screenburn",
+      description:
+        "Logo and brand identity for Screenburn, an organisation researching the implications of excessive screen use.",
+    },
+    {
+      id: "fairwork",
+      description:
+        "Billboard designs for Fairwork, a research group at the Oxford Internet Institute.",
+    },
   ];
 
   return (
@@ -29,7 +57,7 @@ function Design() {
       <p className="py-4 text-2xl lg:mx-5 lg:w-1/3">
         Designs I created in my time at Tapir Studio Limited. I used Adobe
         Photoshop, Illustrator, InDesign and Bridge to create bold, colourful
-        design for a host of clients around the world.
+        designs for a host of clients around the world.
       </p>
       <div className="flex flex-wrap justify-center gap-4 p-4">
         {designs.map((design) => (
@@ -42,7 +70,7 @@ function Design() {
               alt={design.id}
               className="h-auto w-full"
             />
-            {/* <p>{design.description}</p> */}
+            <p className="p-4">{design.description}</p>
           </section>
         ))}
       </div>
