cffc212982ed4fe24cdc1dfc2500069df0ab09a8
jones58 <108466184+jones58@users.noreply.github.com>
2024-01-24 15:14:50 +0000

    edit color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,8 @@ export default {
     extend: {
       colors: {
         // adding my favourite blue, yves klein blue //
-        ykb: {
-          DEFAULT: "#002FA7",
-          50: "#608CFF",
-          100: "#4B7EFF",
-          200: "#2260FF",
-          300: "#0046F9",
-          400: "#003AD0",
-          500: "#002FA7",
-          600: "#001F6F",
-          700: "#000F37",
-          800: "#000000",
-          900: "#000000",
-          950: "#000000",
-        },
+        ykb: "#002FA7",
+        orangepeel: "#FFA630",
       },
     },
   },
