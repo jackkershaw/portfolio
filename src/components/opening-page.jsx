c06d8c26d99f81a8e1f88627087e9168451cab9d
jones58 <108466184+jones58@users.noreply.github.com>
2024-02-27 16:02:23 +0000

    add icons for my tech stack

--- src/components/opening-page.jsx
+++ src/components/opening-page.jsx
@@ -1,9 +1,8 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import MyTechStack from './my-tech-stack';
 
 function OpeningPage() {
-
   return (
-    <div className="mx-10 mb-10 pb-10 sm:py-10 lg:grid lg:grid-cols-3">
+    <div className="mx-10 mb-10 pb-10 sm:grid sm:grid-cols-3 sm:py-10">
       <div className="mt-10 h-auto border-2 border-black p-5 hover:shadow-md hover:shadow-ykb">
         <img
           src="/opening-page/jackkershaw.jpg"
@@ -11,16 +10,13 @@ function OpeningPage() {
           className="h-max-[50vh] w-max-[50vw]"
         />
       </div>
-      <div className="mx-10 mb-10 py-10 text-2xl text-ykb sm:col-span-2 md:text-5xl">
-        <p>Hi there! I'm Jack, a London-based developer and designer.
-I've spent the last two years learning the fundamentals of development,
-including taking part in Just IT's highly selective twelve-week
-bootcamp. So far I've learnt:</p>
-        <FontAwesomeIcon icon={['fab', 'html5']} className="text-orangepeel" />
-        <FontAwesomeIcon icon={['fab', 'css3-alt']} />
-        <FontAwesomeIcon icon={['fab', 'js-square']} />
-        <FontAwesomeIcon icon={['fab', 'react']} />
-
+      <div className="mb-10 py-10 text-3xl text-ykb sm:col-span-2 sm:mx-10 md:text-5xl">
+        <p>
+          Hi there! I'm Jack, a London-based developer and designer. I've spent
+          the last two years learning the fundamentals of development, including
+          taking part in a twelve-week bootcamp. So far I've taught myself:
+        </p>
+        <MyTechStack />
       </div>
     </div>
   );
