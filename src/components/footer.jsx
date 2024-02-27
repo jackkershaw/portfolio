91e5c4ff7f5f3342c8e78ab3fc0faab33a069b5e
jones58 <108466184+jones58@users.noreply.github.com>
2024-02-27 15:24:12 +0000

    add icon svgs

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,49 +1,57 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faGithub,
-  faLinkedin,
-  faFreeCodeCamp,
-} from "@fortawesome/free-brands-svg-icons";
-
+/* NB all SVGs are from Fontawesome: <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */
 function Footer() {
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
-
   return (
-    <>
+    <div>
       <footer className="fixed bottom-0 w-full bg-ykb py-4 text-2xl text-white sm:max-h-20">
         <navbar className="container flex flex-col items-center justify-between sm:mx-5 sm:flex-row lg:mx-10">
           <h2
-            onClick={scrollToTop}
-            className="hover:text-orangepeel  cursor-pointer active:text-white"
+            onClick={() => window.scrollTo(0, 0)}
+            className="cursor-pointer hover:text-orangepeel active:text-white"
           >
-            {" "}
+            {' '}
             back to top ^
           </h2>
           <div className="flex space-x-6 pt-4 sm:pt-0">
             <a href="https://github.com/jones58" target="_blank">
-              <FontAwesomeIcon
-                icon={faGithub}
-                className="hover:text-orangepeel cursor-pointer  text-3xl active:text-white "
-              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                className="h-12 w-12 cursor-pointer hover:text-orangepeel active:text-white"
+              >
+                <path
+                  d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.207 11.387.6.11.793-.262.793-.586 0-.288-.01-1.05-.015-2.06-3.35.73-4.055-1.562-4.055-1.562-.547-1.387-1.336-1.757-1.336-1.757-1.09-.745.082-.73.082-.73 1.207.086 1.843 1.242 1.843 1.242 1.07 1.838 2.807 1.305 3.493.997.108-.777.418-1.306.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.47-2.38 1.242-3.22-.125-.303-.54-1.523.12-3.176 0 0 1.008-.322 3.3 1.23.958-.266 1.98-.398 3-.402 1.02.004 2.042.136 3 .402 2.29-1.552 3.297-1.23 3.297-1.23.66 1.653.246 2.873.12 3.176.77.84 1.24 1.91 1.24 3.22 0 4.608-2.805 5.625-5.475 5.92.43.37.81 1.096.81 2.215 0 1.602-.015 2.887-.015 3.28 0 .328.19.71.8.585C20.565 21.793 24 17.293 24 12c0-6.63-5.37-12-12-12z"
+                  fill="currentColor"
+                />
+              </svg>
             </a>
             <a href="https://www.freecodecamp.com/jones58" target="_blank">
-              <FontAwesomeIcon
-                icon={faFreeCodeCamp}
-                className="hover:text-orangepeel cursor-pointer  text-3xl active:text-white"
-              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 576 512"
+                className="h-12 w-12 cursor-pointer hover:text-orangepeel active:text-white"
+              >
+                <path
+                  d="M97.2 96.2c10.4-10.7 16-17.1 16-21.9 0-2.8-1.9-5.5-3.8-7.4A14.8 14.8 0 0 0 101 64.1c-8.5 0-20.9 8.8-35.8 25.7C23.7 137 2.5 182.8 3.4 250.3s17.5 117 54.1 161.9C76.2 435.9 90.6 448 100.9 448a13.6 13.6 0 0 0 8.4-3.8c1.9-2.8 3.8-5.6 3.8-8.4 0-5.6-3.9-12.2-13.2-20.6-44.5-42.3-67.3-97-67.5-165C32.3 188.8 54 137.8 97.2 96.2zM239.5 420.1c.6 .4 .9 .6 .9 .6zm93.8 .6 .2-.1C333.2 420.6 333.2 420.7 333.3 420.6zm3.1-158.2c-16.2-4.2 50.4-82.9-68.1-177.2 0 0 15.5 49.4-62.8 159.6-74.3 104.4 23.5 168.7 34 175.2-6.7-4.4-47.4-35.7 9.6-128.6 11-18.3 25.5-34.9 43.5-72.2 0 0 15.9 22.5 7.6 71.1C287.7 364 354 342.9 355 343.9c22.8 26.8-17.7 73.5-21.6 76.6 5.5-3.7 117.7-78 33-188.1C360.4 238.4 352.6 266.6 336.4 262.4zM510.9 89.7C496 72.8 483.5 64 475 64a14.8 14.8 0 0 0 -8.4 2.8c-1.9 1.9-3.8 4.7-3.8 7.4 0 4.8 5.6 11.3 16 21.9 43.2 41.6 65 92.6 64.8 154.1-.2 68-23 122.6-67.5 165-9.3 8.4-13.2 14.9-13.2 20.6 0 2.8 1.9 5.6 3.8 8.4A13.6 13.6 0 0 0 475.1 448c10.3 0 24.7-12.1 43.5-35.8 36.6-44.9 53.1-94.4 54.1-161.9S552.3 137 510.9 89.7z"
+                  fill="currentColor"
+                />
+              </svg>
             </a>
             <a href="https://www.linkedin.com/in/jackkershaw" target="_blank">
-              <FontAwesomeIcon
-                icon={faLinkedin}
-                className="hover:text-orangepeel cursor-pointer  text-3xl active:text-white"
-              />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className=" h-12 w-12 cursor-pointer hover:text-orangepeel active:text-white"
+              >
+                <path
+                  d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"
+                  fill="currentColor"
+                />
+              </svg>
             </a>
           </div>
         </navbar>
       </footer>
-    </>
+    </div>
   );
 }
 
