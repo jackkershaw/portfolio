7b62b61ae43be8e680e34f07043d13b3ef80d16a
jones58 <108466184+jones58@users.noreply.github.com>
2024-02-29 15:56:55 +0000

    run prettier on header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,16 +3,28 @@ function Header() {
     <>
       <header className="min-w-100 bg-ykb py-4 text-white">
         <nav className="container flex flex-col items-center justify-between py-4 text-center text-2xl sm:mx-10 sm:flex-row lg:text-3xl">
-          <a href="/" className="hover:text-orangepeel active:text-white">
+          <a
+            href="/"
+            className="hover:text-orangepeel active:text-white"
+          >
             About Me
           </a>
-          <a href="/coding" className="hover:text-orangepeel active:text-white">
+          <a
+            href="/coding"
+            className="hover:text-orangepeel active:text-white"
+          >
             Coding
           </a>
-          <a href="/design" className="hover:text-orangepeel active:text-white">
+          <a
+            href="/design"
+            className="hover:text-orangepeel active:text-white"
+          >
             Design
           </a>
-          <a href="/blog" className="hover:text-orangepeel active:text-white">
+          <a
+            href="/blog"
+            className="hover:text-orangepeel active:text-white"
+          >
             Blog
           </a>
           <a
@@ -21,7 +33,10 @@ function Header() {
           >
             Contact
           </a>
-          <a href="/cv" className="hover:text-orangepeel active:text-white">
+          <a
+            href="/cv"
+            className="hover:text-orangepeel active:text-white"
+          >
             CV
           </a>
         </nav>
