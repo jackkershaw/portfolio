21ffbb3883c41f406507c3c9577db5143068e22f
jones58 <108466184+jones58@users.noreply.github.com>
2024-02-29 16:45:48 +0000

    style header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
+
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       <header className="min-w-100 bg-ykb py-4 text-white">
-        <nav className="container flex flex-col items-center justify-between py-4 text-center text-2xl sm:mx-10 sm:flex-row lg:text-3xl">
+        <nav
+          className={`container items-center justify-between py-4 text-center text-3xl sm:mx-10 space-y-10 sm:space-y-0 ${menuOpen ? "flex sm:flex-row flex-col" : "hidden sm:flex sm:flex-row"}`}
+        >
+          {" "}
           <a
             href="/"
             className="hover:text-orangepeel active:text-white"
@@ -40,6 +47,35 @@ function Header() {
             CV
           </a>
         </nav>
+        <button
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="sm:hidden text-orangepeel hover:text-white focus:outline-none flex flex-row items-center justify-between mx-auto text-2xl"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {menuOpen ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            )}
+          </svg>
+          <h1 className="text-3xl">Menu</h1>
+        </button>
       </header>
     </>
   );
