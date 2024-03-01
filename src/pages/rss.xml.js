739587b1bb8715c53b3bc5c2f1be0d37c1a0642b
jones58 <108466184+jones58@users.noreply.github.com>
2024-03-01 09:54:52 +0000

    edit rss feed and appearance

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,21 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET(context) {
+  const blog = await getCollection("blog");
   return rss({
-    title: 'Jack Kershaw | Blog',
+    title: "Jack Kershaw's Blog",
     description:
-      'My personal blog - writing about the web, typography and design.',
+      "My personal blog - writing about the web, typography and design.",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-gb</language>`,
+    items: blog.map((post) => ({
+      link: post.url,
+      content: sanitizeHtml(post.compiledContent()),
+      ...post.frontmatter,
+    })),
+    stylesheet: "/rss/pretty-feed-v3.xsl",
   });
 }
