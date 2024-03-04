d4e64a00971896b0683e81ceef5c0cad4c746708
jones58 <108466184+jones58@users.noreply.github.com>
2024-03-04 13:17:44 +0000

    edit mobile header look

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,9 +5,38 @@ function Header() {
 
   return (
     <>
-      <header className="min-w-100 bg-ykb py-4 text-white">
+      <header className="min-w-100 bg-ykb pt-6 sm:py-4 text-white">
+        <button
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="sm:hidden text-orangepeel hover:text-white focus:outline-none flex flex-row items-center justify-between mx-auto text-2xl"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {menuOpen ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            )}
+          </svg>
+          <h1 className="text-3xl">Menu</h1>
+        </button>
         <nav
-          className={`container items-center justify-between py-4 text-center text-3xl sm:mx-10 space-y-10 sm:space-y-0 ${menuOpen ? "flex sm:flex-row flex-col" : "hidden sm:flex sm:flex-row"}`}
+          className={`pt-8 sm:pt-0 bg-white text-ykb sm:text-white sm:bg-ykb items-center justify-between text-center text-3xl sm:mx-10 space-y-10 sm:space-y-0 ${menuOpen ? "flex sm:flex-row flex-col" : "hidden sm:flex sm:flex-row"}`}
         >
           {" "}
           <a
@@ -47,35 +76,6 @@ function Header() {
             CV
           </a>
         </nav>
-        <button
-          onClick={() => setMenuOpen(!menuOpen)}
-          className="sm:hidden text-orangepeel hover:text-white focus:outline-none flex flex-row items-center justify-between mx-auto text-2xl"
-        >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            {menuOpen ? (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            ) : (
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              />
-            )}
-          </svg>
-          <h1 className="text-3xl">Menu</h1>
-        </button>
       </header>
     </>
   );
