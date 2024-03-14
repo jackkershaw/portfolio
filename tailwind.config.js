7715e74a54dfb39f15e5bde752053bc4b97525ed
jones58 <108466184+jones58@users.noreply.github.com>
2024-03-14 17:36:24 +0000

    fixing tailwind css typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-
 export default {
-  content: ["./src/**/*.{mjs,js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
-        // adding my favourite blue, yves klein blue //
         ykb: "#002FA7",
         orangepeel: "#FFA630",
       },
       fontFamily: {
-        sans: ["Unica", "Helvetica", "sans-serif"],
-        bold: ["Unica Bold", "Helvetica Bold", "sans-serif"],
+        sans: ["Unica", "sans-serif"],
+        "unica-bold": ["Unica Bold", "sans-serif"],
       },
     },
   },
