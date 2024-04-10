d6a337dd3de53abfc1e833c20773cba9695f9a90
jones58 <108466184+jones58@users.noreply.github.com>
2024-04-10 13:07:33 +0100

    edit opening page to be more professional

--- src/components/opening-page.jsx
+++ src/components/opening-page.jsx
@@ -3,19 +3,19 @@ import MyTechStack from "./my-tech-stack";
 function OpeningPage() {
   return (
     <div className="mx-5 mb-10 pb-10 sm:py-10 md:mx-10 lg:grid lg:grid-cols-3">
-      <div className="mt-10 h-auto border-2 border-black p-5 hover:shadow-md hover:shadow-ykb sm:mt-0">
+      <div className="mt-10 h-[40vh] w-auto sm:h-auto rounded-lg overflow-hidden sm:mt-0">
         <img
           src="/opening-page/jackkershaw.jpg"
           alt="a photo of me. I'm wearing a blue jumper and headphones."
           className="h-auto max-w-full"
         />
       </div>
-      <div className="mb-10 py-5 text-2xl text-ykb sm:col-span-2 sm:mx-5 sm:mb-0 sm:mt-10 sm:py-10 sm:text-3xl md:mx-10 md:text-5xl">
+      <div className="mb-10 py-5 text-2xl text-ykb sm:col-span-2 sm:mx-5 sm:mb-0 sm:mt-10 sm:py-10 sm:text-2xl md:mx-10 md:text-3xl">
         <p>
-          Hi there! I'm Jack, a London-based developer and designer.
-          I've spent the last two years learning the fundamentals of
-          development, including taking part in a twelve-week bootcamp.
-          So far I've learnt:
+          Jack Kershaw is a developer and designer from London. He has
+          been designing for over ten years, and developing websites
+          for the past two years. Depending on the project, his tech
+          stack includes:
         </p>
         <MyTechStack />
       </div>
