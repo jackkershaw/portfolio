08f5c8fca5e5d72babfb96d37786842c97aebb5b
jones58 <108466184+jones58@users.noreply.github.com>
2024-04-10 14:30:39 +0100

    prettier format

--- src/components/cvContent.jsx
+++ src/components/cvContent.jsx
@@ -2,7 +2,7 @@ function CvContent() {
   return (
     <div className="mx-10 mb-10 py-10 pb-10">
       <p className="w-full py-4 text-2xl">
-        Below you'll find my CV. You can also{' '}
+        Below you'll find my CV. You can also{" "}
         <a
           href="Jack-Kershaw-CV.pdf"
           target="_blank"
