7dff7de3a1e1af7428d6efa79e4d4cc77351eaa2
jones58 <108466184+jones58@users.noreply.github.com>
2024-04-17 17:25:41 +0100

    edit footer to not be sticky

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,7 @@
-/* NB all SVGs are from Fontawesome: <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */
 function Footer() {
   return (
     <div>
-      <footer className="sm:fixed bottom-0 w-full bg-ykb py-4 text-2xl text-white sm:max-h-20">
+      <footer className="bottom-0 w-full bg-ykb py-4 text-2xl text-white sm:max-h-20">
         <navbar className="container flex flex-col items-center justify-between sm:mx-5 sm:flex-row lg:mx-10">
           <div className="flex space-x-6 pt-4 sm:pt-0">
             <a href="https://github.com/jones58" target="_blank">
