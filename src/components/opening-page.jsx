bcc9c683dcc101730d2b6828aef8c68ab3927e16
jones58 <108466184+jones58@users.noreply.github.com>
2024-04-22 14:40:09 +0100

    edit opening page appearance

--- src/components/opening-page.jsx
+++ src/components/opening-page.jsx
@@ -1,22 +1,23 @@
 import MyTechStack from "./my-tech-stack";
+import CodingContent from "./codingContent";
 
 function OpeningPage() {
   return (
-    <div className="mb-10 pb-10 lg:grid lg:grid-cols-3">
-      <img
-        src="/opening-page/jackkershaw.jpg"
-        alt="a photo of me. I'm wearing a blue jumper and headphones."
-        className="rounded-lg my-5 sm:my-0"
-      />
-      <div className="mb-10 py-5 text-2xl sm:col-span-2 sm:mx-5 sm:mb-0 sm:mt-10 sm:py-10 sm:text-2xl md:mx-10 md:text-3xl sm:w-3/4">
-        <p>
+    <div>
+      <div className="flex sm:flex-row justify-between flex-col space-y-6">
+        <img
+          src="/opening-page/jackkershaw.jpg"
+          className="sm:max-w-[10vw] h-auto rounded-lg object-cover h-auto"
+        />
+        <p className="text-3xl sm:max-w-[30vw] sm:ml-5">
           Jack Kershaw is a developer and designer from London. He has
           been designing for over ten years, and developing websites
-          for the past two years. Depending on the project, his tech
-          stack includes:
+          for the past two years.
         </p>
         <MyTechStack />
       </div>
+      <div className="w-full h-20 py-10" />
+      <CodingContent />
     </div>
   );
 }
