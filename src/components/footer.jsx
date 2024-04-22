38338162cc3076f95064a3296d98d1da91db9e35
jones58 <108466184+jones58@users.noreply.github.com>
2024-04-22 12:32:57 +0100

    rejig footer and header layout

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,27 +1,7 @@
 function Footer() {
   return (
     <div>
-      <footer className="bottom-0 w-full bg-ykb py-4 text-2xl text-white sm:max-h-20">
-        <navbar className="container flex flex-col items-center justify-between sm:mx-5 sm:flex-row lg:mx-10">
-          <div className="flex space-x-6 pt-4 sm:pt-0">
-            <a href="https://github.com/jones58" target="_blank">
-              <span class="icon-[mdi--github] w-10 h-10 cursor-pointer hover:text-orangepeel active:text-white"></span>
-            </a>
-            <a
-              href="https://www.freecodecamp.com/jones58"
-              target="_blank"
-            >
-              <span class="icon-[simple-icons--freecodecamp] w-11 h-11 cursor-pointer hover:text-orangepeel active:text-white"></span>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/jackkershaw"
-              target="_blank"
-            >
-              <span class="icon-[mdi--linkedin] w-11 h-11 cursor-pointer hover:text-orangepeel active:text-white"></span>
-            </a>
-          </div>
-        </navbar>
-      </footer>
+      <footer className="bottom-0 w-full bg-ykb py-4 text-2xl text-white sm:max-h-20"></footer>
     </div>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ function Header() {
 
   return (
     <>
-      <header className="min-w-100 bg-ykb pt-6 sm:py-6 text-white">
+      <header className="min-w-300 bg-ykb pt-6 sm:py-6 text-white">
         <button
           onClick={() => setMenuOpen(!menuOpen)}
           className="sm:hidden text-white hover:text-white focus:outline-none flex flex-row items-center justify-between mx-auto text-2xl"
@@ -36,33 +36,55 @@ function Header() {
           <h1 className="text-3xl">Menu</h1>
         </button>
         <nav
-          className={` pt-8 sm:pt-0 text-orangepeel sm:text-white sm:slate-600 items-center justify-center space-x-5 text-center text-2xl sm:mx-10 space-y-10 sm:space-y-0 ${menuOpen ? "flex sm:flex-row flex-col" : "hidden sm:flex sm:flex-row"}`}
+          className={`pt-8 sm:pt-0 text-white sm:slate-600 items-center justify-start space-x-5 text-center text-2xl sm:mx-10 space-y-10 sm:space-y-0 ${
+            menuOpen
+              ? "flex sm:flex-row flex-col"
+              : "hidden sm:flex sm:flex-row"
+          }`}
         >
           {" "}
           <a
             href="/"
-            className=" active:text-white sm: sm:hover:bg-ykbdark sm:active:bg-ykblight p-5 rounded-lg"
+            className="active:text-white sm:hover:bg-ykbdark sm:active:bg-ykblight p-5 rounded-lg w-30"
           >
-            About
+            <h1>Jack</h1>
           </a>
           <a
             href="/coding"
-            className=" active:text-white sm: sm:hover:bg-ykbdark sm:active:bg-ykblight p-5 rounded-lg"
+            className="active:text-white sm:hover:bg-ykbdark sm:active:bg-ykblight p-5 rounded-lg w-30"
           >
             Coding
           </a>
           <a
             href="/design"
-            className=" active:text-white sm: sm:hover:bg-ykbdark sm:active:bg-ykblight p-5 rounded-lg"
+            className="active:text-white sm:hover:bg-ykbdark sm:active:bg-ykblight p-5 rounded-lg w-30"
           >
             Design
           </a>
           <a
             href="/blog"
-            className=" active:text-white sm: sm:hover:bg-ykbdark sm:active:bg-ykblight p-5 rounded-lg"
+            className="active:text-white sm:hover:bg-ykbdark sm:active:bg-ykblight p-5 rounded-lg w-30"
           >
             Blog
           </a>
+          <div className="container flex flex-col items-center justify-between sm:mx-5 sm:flex-row lg:mx-10">
+            <div className="flex space-x-6 pt-4 sm:pt-0 ml-auto align-items-center">
+              {" "}
+              {/* Added ml-auto to push icons to the right */}
+              <a href="https://github.com/jones58" target="_blank">
+                <span className="icon-[mdi--github] w-10 h-10 cursor-pointer hover:text-orangepeel active:text-white"></span>
+              </a>
+              <a href="/cv" target="_blank">
+                <span className="icon-[mdi--resume] w-10 h-10 cursor-pointer hover:text-orangepeel active:text-white"></span>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/jackkershaw"
+                target="_blank"
+              >
+                <span className="icon-[mdi--linkedin] w-11 h-11 cursor-pointer hover:text-orangepeel active:text-white"></span>
+              </a>
+            </div>
+          </div>
         </nav>
       </header>
     </>
