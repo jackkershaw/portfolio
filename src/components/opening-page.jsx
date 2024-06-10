ac8f150c07d5edbb8a00f4c6321e4abc6822bf0c
jones58 <108466184+jones58@users.noreply.github.com>
2024-06-10 15:23:58 +0100

    edit content of opening page

--- src/components/opening-page.jsx
+++ src/components/opening-page.jsx
@@ -1,16 +1,12 @@
-import MyTechStack from "./my-tech-stack";
-import CodingContent from "./codingContent";
-
 function OpeningPage() {
   return (
     <div>
       <div className="flex sm:flex-row justify-between flex-col space-y-6">
-        <p className="text-3xl sm:max-w-[30vw] sm:px-5">
+        <p className="text-3xl sm:max-w-[30vw]">
           Jack Kershaw is a developer and designer from London. He has
           been designing for over ten years, and developing websites
           for the past two years.
         </p>
-        <MyTechStack />
       </div>
       <div className="w-full h-20 py-10" />
     </div>
