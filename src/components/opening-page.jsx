99811e23c43d5809fee0373263d596943707a398
jones58 <108466184+jones58@users.noreply.github.com>
2024-07-23 16:39:14 +0100

    edit opening page look

--- src/components/opening-page.jsx
+++ src/components/opening-page.jsx
@@ -1,16 +1,32 @@
 function OpeningPage() {
   return (
     <div>
-      <div className="flex sm:flex-row justify-start flex-col space-y-6 pb-10">
+      <div className="flex sm:flex-row justify-start flex-col py-10">
         <img
           src="public/opening-page/jackkershaw.jpg"
-          className="w-96 h-auto object-cover rounded-lg"
+          className="sm:w-[30vw] h-auto object-cover object-center rounded-lg"
         ></img>
-        <p className="text-4xl sm:max-w-[40vw] sm:p-10 prose">
-          Hi! I'm a developer and designer from London. I have been
-          designing for over ten years, and developing websites for the
-          past two years.
-        </p>
+        <div className="sm:px-14 flex flex-col justify-between sm:max-w-[40vw]">
+          <p className="text-3xl py-14 sm:py-0">
+            Hi! I'm a developer and designer from London. I have been
+            designing for over ten years, and developing websites for
+            the past two years.
+          </p>
+          <div className="flex flex-col justify-between space-y-4">
+            <a
+              href="/contact"
+              className=" bg-ykb hover:bg-ykbdark active:bg-ykblight text-neutral-200 rounded-lg py-5 px-4 no-underline text-center"
+            >
+              Get In Touch
+            </a>
+            <a
+              href="/coding"
+              className=" bg-ykb hover:bg-ykbdark active:bg-ykblight text-neutral-200 rounded-lg py-5 px-4 no-underline text-center"
+            >
+              See Recent Work
+            </a>
+          </div>
+        </div>
       </div>{" "}
     </div>
   );
