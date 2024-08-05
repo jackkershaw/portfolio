69c51e6ac181e5b0c0d754c4affbfcc775f24719
jones58 <108466184+jones58@users.noreply.github.com>
2024-08-05 11:52:35 +0100

    edit descriptions

--- src/components/codingContent.jsx
+++ src/components/codingContent.jsx
@@ -26,16 +26,9 @@ function CodingContent() {
       title: "Fare Free London",
       description: [
         "Freelance project for a public transport campaign.",
-        "Gathered inspiration in an Are.na folder, creating moodboards and wireframes in Figma.",
-        "Set up WordPress as CMS on a subdomain of the main site.",
-        "Used Responsively to improve the responsive design.",
-        "Used Back To Top and Get Involved buttons to improve user navigation and drive traffic.",
+        "Used Next.js and Headless Wordpress to build a fast, secure and easy to update site.",
         "Debugged an issue with the security of the signup form, moving requests to the server-side through an API route to Airtable.",
-        "Implemented Deploy hooks with Vercel and Wordpress Webhooks for automatic site rebuilds.",
-        "Used Unlighthouse to improve the SEO, perfomance and accessibility of all pages on the site.",
-        "Improved Largest Contentful Paint (LCP) from 11.3s to 2.8s by using Next/Image for image optimization, dynamic imports for lazy loading, and optimizing images with the 'sharp' package. This boosted the Lighthouse performance score from 67 to 96.",
-        "I enhanced SEO with meta tags, canonical tags, and Google Search Console integration.",
-        "Integrated self-hosted Umami analytics for tracking.",
+        "Improved Lighthouse performance score from 67 to 96 by using Next/Image for image optimisation and dynamic imports for lazy loading.",
         "Responded to public user feedback in Github Issues.",
       ],
       link: "https://www.farefreelondon.org/",
@@ -46,11 +39,8 @@ function CodingContent() {
       title: "Right to the City",
       description: [
         "Freelance project for a festival in London.",
-        "Set up animations using Framer Motion.",
-        "Added some template event data in JSON, made an event card component and added it to the events section using .map() to go through the data and create a new card for each.",
         "Implemented a custom Tailwind utility for variable font settings.",
-        "Built a form using Tailwind and Material Design principles, and linked it to an Airtable spreadsheet, debugging the API using Postman and adjusting the JSON format as necessary.",
-        "Improved accessibility and SEO with Google Lighthouse and WCAG tools, and replaced alerts with SweetAlert2 for clearer user experience and accessibility.",
+        "Built a sign up form using Material Design principles, linked it to an Airtable spreadsheet. This involved debugging the JSON data using Postman.",
         "Added a back to top button to improve navigation on the site.",
       ],
       link: "https://right-to-city.netlify.app/",
@@ -61,13 +51,9 @@ function CodingContent() {
       title: "Vital Signs",
       description: [
         "Freelance project for a group of NHS workers.",
-        "Used Next.js and Headless Wordpress to build a fast, secure and easy to update site.",
-        "Used a Vercel Next.js and Wordpress example to bootstrap the project, and set up WordPress hosting with GraphQL as the API.",
-        "Used dummy content for testing, addressing issues like image errors and font loading by referring to the Next.js docs, adjusting configuration files and using Next/font.",
-        "Used Figma to create the design of the site and implemented it with Tailwind and Iconify, adding animations with Framer Motion.",
-        "Improved SEO using the Next.js docs, OpenGraph.xyz to test social share links and the NPM package next-sitemap.",
-        "Used Advanced Custom Fields to create a PDF uploader in the Wordpress editor and displayed this as an embed, with an anchor tag to view the pdf full screen.",
-        "Created a guide for the client to help them edit site content easily and resolved their issues with PDF loading on mobile browsers.",
+        "Used dummy content for testing, addressing issues like image errors and font loading by referring to the Next.js docs.",
+        "Used Advanced Custom Fields to create a PDF uploader in the Wordpress editor.",
+        "Created a guide for the client to help them edit site content easily.",
         "Used Wordpress webhooks connected to Vercel Deploy Hooks to trigger a site rebuild when posts are created, updated or deleted.",
       ],
       link: "https://www.vitalsignsmag.org/",
@@ -77,13 +63,11 @@ function CodingContent() {
     {
       title: "London Brutalist Map",
       description: [
-        "Rebuilt previous HTML and JavaScript project in React, for improved developed and user experience.",
-        "Transitioning from the Mapbox CDN to the react-map-gl NPM package enabled me to use the GEOJSON data locally, improving the editing process.",
-        "Deployed a multi-page site which enhanced the user experience by offering various ways to engage with the data.",
-        "Implemented React Router for navigation and page management, including a 404 page to handle missing routes.",
+        "Rebuilt my previous HTML project in React, for improved developed and user experience.",
+        "Deployed a multi-page site which offers various ways to engage with the data.",
+        "Implemented React Router for navigation and page management.",
         "Learnt to use APIs with JavaScript’s fetch() and applied this knowledge to React.",
         "Responded to user feedback by adding a custom marker to handle hover events.",
-        "Implemented state management to control marker interactions.",
       ],
       link: "https://brutalistmap.jackkershaw.net/",
       github_link: "https://github.com/jones58/brutalist-map-2/",
@@ -105,11 +89,9 @@ function CodingContent() {
     {
       title: "Typespotting",
       description: [
-        "Built this after noticing a gap in existing typography games, which typically focus on multiple-choice questions.",
-        "Used a mind map to come up with the idea for the project (Where's Wally but for Cooper Black)",
-        "Categorised images into difficulty levels, and applied a bold, contrasting colour scheme.",
+        "Used a mind map of existing typography games to come up with the idea for the project (Where's Wally but for Cooper Black).",
         "Created SVG graphics using Adobe Illustrator and optimised images with an Image Optimiser app, ensuring the game was responsive and fast.",
-        "Wrote pseudocode to plan complex JavaScript functions for randomising colour schemes and managing click events.",
+        "Wrote pseudocode to plan complex JavaScript functions, for randomising colour schemes and managing click events.",
       ],
       link: "https://typespotting.jackkershaw.net/",
       github_link: "https://github.com/jones58/typespotting/",
@@ -176,7 +158,7 @@ function CodingContent() {
                     {project.description.map((paragraph, index) => (
                       <li
                         key={index}
-                        className="text-neutral-900 py-2 text-xl"
+                        className="text-neutral-900 py-1 text-xl"
                       >
                         {paragraph}
                       </li>
