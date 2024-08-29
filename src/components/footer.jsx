92dd77566b1c06f0045819ff71ceebbbcf5d0ea7
jones58 <108466184+jones58@users.noreply.github.com>
2024-08-29 17:35:09 +0100

    edit footer styling

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,19 +1,11 @@
 function Footer() {
   return (
-    <footer className="bottom-0 bg-ykb py-10 text-2xl text-neutral-100">
-      <div className="mx-10">
-        <div className="flex-col sm:justify-end flex pt-10 sm:pt-0">
-          <div className="flex flex-row justify-center sm:justify-end space-x-5 text-right py-5">
-            {" "}
+    <footer className="relative w-full bottom-0 bg-ykb text-2xl text-neutral-100 py-4">
+      <div className="mx-10 h-full">
+        <div className="flex flex-col justify-center h-full">
+          <div className="flex flex-row justify-between sm:justify-end items-center space-x-5 text-center">
             <a href="https://github.com/jones58" target="_blank">
               <span className="icon-[mdi--github] w-20 h-20 cursor-pointer hover:bg-ykbdark active:text-ykblight"></span>
-            </a>
-            <a
-              href="Jack-Kershaw-CV.pdf"
-              target="_blank"
-              download="Jack-Kershaw-CV.pdf"
-            >
-              <span className="icon-[mdi--resume] w-20 h-20 cursor-pointer hover:bg-ykbdark active:text-ykblight"></span>
             </a>
             <a
               href="https://www.linkedin.com/in/jackkershaw"
