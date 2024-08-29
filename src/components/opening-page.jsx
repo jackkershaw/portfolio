a28a5d6f3c89a7943018a28d0a8691370c5c3f00
jones58 <108466184+jones58@users.noreply.github.com>
2024-08-29 17:35:32 +0100

    edit opening page style

--- src/components/opening-page.jsx
+++ src/components/opening-page.jsx
@@ -1,14 +1,14 @@
 function OpeningPage() {
   return (
     <div>
-      <div className="flex sm:flex-row justify-between flex-col py-10">
+      <div className="flex sm:flex-row justify-between sm:justify-center sm:space-x-10 flex-col">
         <img
           src="/opening-page/jackkershaw.jpg"
           className="sm:w-[30vw] h-auto object-cover object-center rounded-lg"
           alt="Jack wearing a blue jumper and black headphones, peering over his laptop at the camera"
         ></img>
-        <div className="sm:px-14 flex flex-col justify-between sm:max-w-4xl">
-          <p className="text-3xl py-14 sm:py-0">
+        <div className=" flex flex-col justify-between sm:max-w-2xl">
+          <p className="text-xl sm:text-2xl py-14 sm:py-0">
             Hi there! I'm a frontend developer based in London. With
             over ten years of design experience and two years in
             development, I’m passionate about creating beautiful and
