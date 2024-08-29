2e1495415936eaa13f3a096022ca38f6947402e6
jones58 <108466184+jones58@users.noreply.github.com>
2024-08-29 17:35:14 +0100

    edit header padding

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ function Header() {
 
   return (
     <>
-      <header className="bg-ykb py-5 text-neutral-100 relative">
+      <header className="bg-ykb py-4 sm:py-2 text-neutral-100 relative">
         <button
           onClick={() => setMenuOpen(!menuOpen)}
           className="sm:hidden focus:outline-none flex flex-row items-center justify-between mx-auto text-4xl"
@@ -24,7 +24,7 @@ function Header() {
           <h1 className="text-4xl px-10">Jack Kershaw</h1>
         </button>
         <nav
-          className={`absolute left-0 right-0 top-full bg-ykb py-4 sm:relative sm:top-0 sm:bg-transparent transition-all duration-300 ease-in-out transform ${
+          className={`absolute left-0 right-0 py-4 sm:py-0 top-full bg-ykb sm:relative sm:top-0 sm:bg-transparent transition-all duration-300 ease-in-out transform ${
             menuOpen
               ? "opacity-100 translate-y-0 "
               : "opacity-0 -translate-y-full"
@@ -35,7 +35,7 @@ function Header() {
           }`}
         >
           <a
-            className="hidden text-4xl sm:block active:text-neu sm:hover:bg-ykbdark sm:active:bg-ykblight sm:p-5 rounded-lg w-30"
+            className="hidden text-4xl sm:block sm:p-5 rounded-lg w-30"
             href="/"
           >
             Jack Kershaw
