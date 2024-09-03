a0feafd8a55e569973f6dbab196fa31c7e24cacd
jones58 <108466184+jones58@users.noreply.github.com>
2024-09-03 13:11:25 +0100

    edit description of projects

--- src/components/codingContent.jsx
+++ src/components/codingContent.jsx
@@ -16,7 +16,7 @@ function CodingContent() {
     {
       title: "Take Back Water",
       description: [
-        "Maintained the Take Back Water website within a team of developers.",
+        "Employed as a contractor within a team of developers.",
         "Worked on accessibility and performance, reaching a score of 100 across all categories on Lighthouse.",
         "Developed and tested new React components with Storybook and SASS.",
         "Upgraded the SEO of the site with meta tags.",
