6745424377ad9c7e1cd74a76e37b8c119f71c4a7
jones58 <108466184+jones58@users.noreply.github.com>
2024-12-05 12:48:18 +0000

    fix footer spacing and alignment of items inside

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,24 +1,24 @@
 function Footer() {
   return (
-    <footer className="relative w-full bottom-0 bg-ykb text-2xl text-neutral-100 py-4">
+    <footer className="relative w-full bottom-0 bg-ykb text-neutral-100 py-5 sm:py-2.5">
       <div className="mx-10 h-full">
-        <div className="flex flex-col justify-center h-full">
-          <div className="flex flex-row justify-between sm:justify-end items-center space-x-5 text-center">
-            <a
-              href="https://github.com/jones58"
-              target="_blank"
-              aria-label="Jack Kershaw Github"
-            >
-              <span className="icon-[mdi--github] w-20 h-20 cursor-pointer hover:bg-ykbdark active:text-ykblight"></span>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/jackkershaw"
-              target="_blank"
-              aria-label="Jack Kershaw LinkedIn"
-            >
-              <span className="icon-[mdi--linkedin] w-20 h-20 cursor-pointer hover:bg-ykbdark active:text-ykblight"></span>
-            </a>
-          </div>
+        <div className="flex flex-col sm:flex-row justify-center sm:justify-end items-center h-full rounded-full">
+          <a
+            href="https://github.com/jones58"
+            target="_blank"
+            aria-label="Jack Kershaw Github"
+            className="flex items-center justify-center rounded-full"
+          >
+            <span className="icon-[mdi--github] w-20 h-20 cursor-pointer hover:bg-ykbdark active:text-ykblight"></span>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/jackkershaw"
+            target="_blank"
+            aria-label="Jack Kershaw LinkedIn"
+            className="flex items-center justify-center rounded-full"
+          >
+            <span className="icon-[mdi--linkedin] w-20 h-20 cursor-pointer hover:bg-ykbdark active:text-ykblight"></span>
+          </a>
         </div>
       </div>
     </footer>
