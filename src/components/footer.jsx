9da072644387c6ce24fe797200ca7fe93c24ef67
jones58 <108466184+jones58@users.noreply.github.com>
2024-12-05 11:52:14 +0000

    improve accessibility with aria label

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,12 +4,17 @@ function Footer() {
       <div className="mx-10 h-full">
         <div className="flex flex-col justify-center h-full">
           <div className="flex flex-row justify-between sm:justify-end items-center space-x-5 text-center">
-            <a href="https://github.com/jones58" target="_blank">
+            <a
+              href="https://github.com/jones58"
+              target="_blank"
+              aria-label="Jack Kershaw Github"
+            >
               <span className="icon-[mdi--github] w-20 h-20 cursor-pointer hover:bg-ykbdark active:text-ykblight"></span>
             </a>
             <a
               href="https://www.linkedin.com/in/jackkershaw"
               target="_blank"
+              aria-label="Jack Kershaw LinkedIn"
             >
               <span className="icon-[mdi--linkedin] w-20 h-20 cursor-pointer hover:bg-ykbdark active:text-ykblight"></span>
             </a>
