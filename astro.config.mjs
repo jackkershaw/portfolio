36a5819b672f2fbcbc35efe009c863a154c17735
jones58 <108466184+jones58@users.noreply.github.com>
2024-12-11 12:16:24 +0000

    na

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
